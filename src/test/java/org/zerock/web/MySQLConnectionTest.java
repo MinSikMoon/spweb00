bf59bc59e736f131228f0eb137dc8d42467d1387
xcsxcs <xcsxcs@DESKTOP-SQ09Q4F>
2017-04-21 22:34:12 +0900

    스프링 4.3.8버전으로 수정 완료

--- src/test/java/org/zerock/web/MySQLConnectionTest.java
+++ src/test/java/org/zerock/web/MySQLConnectionTest.java
@@ -7,16 +7,11 @@ import org.junit.Test;
 
 public class MySQLConnectionTest {
 
-	private static final String DRIVER = 
-			//MySQL Driver 5 이하 
-			//"com.mysql.jdbc.Driver"
-			//MySQL Driver 6 이상 
-			"com.mysql.cj.jdbc.Driver";
-	private static final String URL =
-			//MySQL 5.6 이하 
-			//"jdbc:mysql://127.0.0.1:3306/book_ex";
-			//MySQL 5.7 이상 
-			"jdbc:mysql://127.0.0.1:3306/book_ex?useSSL=false&serverTimezone=UTC";
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	//MySQL Driver 6.0 이상	"com.mysql.cj.jdbc.Driver";
+	
+	private static final String URL = "jdbc:mysql://127.0.0.1:3306/book_ex?useSSL=false";
+	//MySQL 6.1 이상 "jdbc:mysql://127.0.0.1:3306/book_ex?useSSL=false&serverTimezone=Asia/Seoul";
 	
 	private static final String USER = 
 			"zerock";
